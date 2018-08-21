2a9c7461bc82486f433a9e8dde5a943067425ae9
David Furnes <david@dfurnes.com>
2018-08-21 15:17:31 -0400

    Pin provider & add a backend.

--- main.tf
+++ main.tf
@@ -1,8 +1,20 @@
+provider "fastly" {
+  version = "~> 0.3"
+}
+
 resource "fastly_service_v1" "misc" {
   name = "Terraform: Miscellaneous"
 
   domain {
     name = "api.dosomething.org"
+  }
+
+  # Note: Fastly requires at least one backend per service,
+  # so our AWS HAProxy instance is included here.
+  backend {
+    address = "54.172.90.245"
+    name = "HAProxy"
+    port = 80
   }
 
   vcl {
