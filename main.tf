adda17bc521c13b402c2e91d928c744e87c324b6
David Furnes <david@dfurnes.com>
2018-08-22 15:45:07 -0400

    Add production Voting App service.

--- main.tf
+++ main.tf
@@ -2,6 +2,44 @@ provider "fastly" {
   version = "~> 0.3"
 }
 
+resource "fastly_service_v1" "voting-app" {
+  name = "Terraform: Voting App"
+
+  domain {
+    name = "www.celebsgonegood.com"
+  }
+
+  domain {
+    name = "www.athletesgonegood.com"
+  }
+
+  domain {
+    name = "www.fourleggedfinishers.com"
+  }
+
+  backend {
+    address = "54.172.90.245"
+    name = "HAProxy"
+    port = 80
+  }
+
+  header {
+    name = "Country Code"
+    type = "request"
+    action = "set"
+    source = "geoip.country_code"
+    destination = "http.X-Fastly-Country-Code"
+  }
+
+  header {
+    name = "Country Code (Debug)"
+    type = "response"
+    action = "set"
+    source = "geoip.country_code"
+    destination = "http.X-Fastly-Country-Code"
+  }
+}
+
 resource "fastly_service_v1" "voting-app-qa" {
   name = "Terraform: Voting App (QA)"
 
