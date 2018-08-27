e7d20d7a822678a0726f706427d93e905b4f8556
David Furnes <david@dfurnes.com>
2018-08-27 17:31:07 -0400

    Add Thor HAProxy box.

--- dosomething-qa/ashes/main.tf
+++ dosomething-qa/ashes/main.tf
@@ -11,6 +11,21 @@ resource "aws_instance" "staging" {
   }
 }
 
+resource "aws_instance" "haproxy_qa" {
+  ami = "ami-d05e75b8"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "Thor-HA-Proxy"
+    Role = "Thor-HA-Proxy"
+    Environment = "Thor"
+  }
+}
+
 output "backend_staging" {
   value = "${aws_instance.staging.public_ip}"
+}
+
+output "backend_haproxy_qa" {
+  value = "${aws_instance.haproxy_qa.public_ip}"
 }
