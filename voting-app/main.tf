6b6043d083c8354fad8ef5102f7616889ee985e4
David Furnes <david@dfurnes.com>
2018-08-29 12:26:09 -0400

    Create buckets for static voting app sites.

--- voting-app/main.tf
+++ voting-app/main.tf
@@ -36,3 +36,29 @@ resource "fastly_service_v1" "voting-app" {
     destination = "http.X-Fastly-Country-Code"
   }
 }
+
+resource "aws_s3_bucket" "cgg" {
+  bucket = "www.celebsgonegood.com"
+  acl    = "public-read"
+
+  # see: https://docs.aws.amazon.com/AmazonS3/latest/dev/WebsiteAccessPermissionsReqd.html 
+  policy = "${file("${path.module}/policy-cgg.json")}"
+
+  website {
+    index_document = "index.html"
+    error_document = "error.html"
+  }
+}
+
+resource "aws_s3_bucket" "agg" {
+  bucket = "www.athletesgonegood.com"
+  acl    = "public-read"
+
+  # see: https://docs.aws.amazon.com/AmazonS3/latest/dev/WebsiteAccessPermissionsReqd.html 
+  policy = "${file("${path.module}/policy-agg.json")}"
+
+  website {
+    index_document = "index.html"
+    error_document = "error.html"
+  }
+}
