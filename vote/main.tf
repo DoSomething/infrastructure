06f911bfd5b23a5b76db00ca8087514ab9bc7a58
David Furnes <david@dfurnes.com>
2018-09-21 13:27:51 -0400

    Add test pass-through Fastly property.

---
+++ vote/main.tf
@@ -0,0 +1,43 @@
+resource "fastly_service_v1" "vote" {
+  name          = "Terraform: Voter Registration"
+  force_destroy = true
+
+  domain {
+    name = "vote-fastly.dosomething.org"
+  }
+
+  default_host = "vote.dosomething.org"
+
+  backend {
+    name    = "instapage"
+    address = "secure.pageserve.co"
+    port    = 80
+  }
+
+  gzip {
+    name = "gzip"
+
+    extensions = ["css", "js", "html", "eot", "ico", "otf", "ttf", "json"]
+
+    content_types = [
+      "text/html",
+      "application/x-javascript",
+      "text/css",
+      "application/javascript",
+      "text/javascript",
+      "application/json",
+      "application/vnd.ms-fontobject",
+      "application/x-font-opentype",
+      "application/x-font-truetype",
+      "application/x-font-ttf",
+      "application/xml",
+      "font/eot",
+      "font/opentype",
+      "font/otf",
+      "image/svg+xml",
+      "image/vnd.microsoft.icon",
+      "text/plain",
+      "text/xml",
+    ]
+  }
+}
