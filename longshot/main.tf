3fc426c4a2e5c85c96a60dd10189526bfcd15234
David Furnes <david@dfurnes.com>
2018-11-16 17:11:25 -0500

    Create Footlocker Internal instance.

--- longshot/main.tf
+++ longshot/main.tf
@@ -22,3 +22,20 @@ module "longshot-qa" {
 
   papertrail_destination = "${var.papertrail_qa_destination}"
 }
+
+module "longshot-footlocer-internal" {
+  source = "application"
+
+  name           = "longshot-footlocker-internal"
+  host           = "footlocker-internal.dosomething.org"
+  pipeline       = "${heroku_pipeline.longshot.id}"
+  pipeline_stage = "production"
+
+  email_name    = "Foot Locker Scholar Athletes"
+  email_address = "footlocker@tmiagency.org"
+
+  database_name = "footlocker_internal"
+  database_type = "db.t2.medium"
+
+  papertrail_destination = "${var.papertrail_prod_destination}"
+}
