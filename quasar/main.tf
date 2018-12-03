9e456e5372681685b10f1733f0b1428854c8c168
Sena Heydari <sena.heydari@gmail.com>
2018-12-03 11:48:03 -0500

    Modifying true for booleans.

--- quasar/main.tf
+++ quasar/main.tf
@@ -242,9 +242,9 @@ resource "aws_db_instance" "quasar-qa" {
   password               = "${data.aws_ssm_parameter.qa_password.value}"
   parameter_group_name   = "${aws_db_parameter_group.quasar-qa.id}"
   vpc_security_group_ids = ["${aws_security_group.rds.id}"]
-  deletion_protection    = "true"
-  storage_encrypted      = "true"
-  copy_tags_to_snapshot  = "true"
+  deletion_protection    = true
+  storage_encrypted      = true
+  copy_tags_to_snapshot  = true
   monitoring_interval    = "60"
-  publicly_accessible    = "true"
+  publicly_accessible    = true
 }
