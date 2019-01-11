142f59c73f8ae11105742e68f57dd8a139706bc5
David Furnes <david@dfurnes.com>
2019-01-11 11:16:24 -0500

    Use a PGP key pair to encrypt MySQL account passwords in state.

--- shared/mariadb_instance/main.tf
+++ shared/mariadb_instance/main.tf
@@ -142,8 +142,12 @@ resource "mysql_user" "readonly" {
 }
 
 resource "mysql_user_password" "readonly" {
-  user    = "${mysql_user.readonly.user}"
-  pgp_key = "keybase:dfurnes"
+  user = "${mysql_user.readonly.user}"
+
+  # This resource automatically generates a password for the MySQL
+  # user, encrypted by a PGP public key. The corresponding private
+  # key & decryption instructions can be found in Lastpass.
+  pgp_key = "${base64encode(file("${path.root}/shared/pgp/public.key"))}"
 }
 
 resource "mysql_grant" "readonly" {
