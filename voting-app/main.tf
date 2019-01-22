dc9359b440d1147575c04dcc6c200ace9e0300a1
David Furnes <david@dfurnes.com>
2019-01-22 17:17:51 -0500

    Remove CGG from Voting App config.

--- voting-app/main.tf
+++ voting-app/main.tf
@@ -3,23 +3,7 @@ resource "fastly_service_v1" "voting-app" {
   force_destroy = true
 
   domain {
-    name = "www.celebsgonegood.com"
-  }
-
-  domain {
     name = "www.athletesgonegood.com"
-  }
-
-  condition {
-    type      = "REQUEST"
-    name      = "backend-celebsgonegood"
-    statement = "req.http.host == \"www.celebsgonegood.com\""
-  }
-
-  condition {
-    type      = "REQUEST"
-    name      = "backend-athletesgonegood"
-    statement = "req.http.host == \"www.athletesgonegood.com\""
   }
 
   gzip {
@@ -50,30 +34,9 @@ resource "fastly_service_v1" "voting-app" {
   }
 
   backend {
-    name              = "s3-www.celebsgonegood.com"
-    address           = "${aws_s3_bucket.cgg.bucket}.s3-website-${aws_s3_bucket.cgg.region}.amazonaws.com"
-    request_condition = "backend-celebsgonegood"
-    port              = 80
-  }
-
-  backend {
-    name              = "s3-www.athletesgonegood.com"
-    address           = "${aws_s3_bucket.agg.bucket}.s3-website-${aws_s3_bucket.agg.region}.amazonaws.com"
-    request_condition = "backend-athletesgonegood"
-    port              = 80
-  }
-}
-
-resource "aws_s3_bucket" "cgg" {
-  bucket = "www.celebsgonegood.com"
-  acl    = "public-read"
-
-  # see: https://docs.aws.amazon.com/AmazonS3/latest/dev/WebsiteAccessPermissionsReqd.html 
-  policy = "${file("${path.module}/policy-cgg.json")}"
-
-  website {
-    index_document = "index.html"
-    error_document = "error.html"
+    name    = "s3-www.athletesgonegood.com"
+    address = "${aws_s3_bucket.agg.bucket}.s3-website-${aws_s3_bucket.agg.region}.amazonaws.com"
+    port    = 80
   }
 }
 
