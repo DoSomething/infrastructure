875ebbeeaef7fab197f636718712676442573597
David Furnes <david@dfurnes.com>
2019-01-25 15:46:20 -0500

    Set API gateway timeout.

--- shared/lambda_function/main.tf
+++ shared/lambda_function/main.tf
@@ -63,6 +63,9 @@ resource "aws_api_gateway_integration" "lambda_root" {
   resource_id = "${aws_api_gateway_method.proxy_root.resource_id}"
   http_method = "${aws_api_gateway_method.proxy_root.http_method}"
 
+  # TODO: Calculate this from Lambda timeout.
+  timeout_milliseconds = 15000
+
   # Lambda functions can only be invoked via POST.
   integration_http_method = "POST"
   type                    = "AWS_PROXY"
@@ -80,6 +83,9 @@ resource "aws_api_gateway_integration" "lambda" {
   rest_api_id = "${aws_api_gateway_rest_api.gateway.id}"
   resource_id = "${aws_api_gateway_method.proxy.resource_id}"
   http_method = "${aws_api_gateway_method.proxy.http_method}"
+
+  # TODO: Calculate this from Lambda timeout.
+  timeout_milliseconds = 15000
 
   # Lambda functions can only be invoked via POST.
   integration_http_method = "POST"
