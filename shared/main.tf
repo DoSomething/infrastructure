58c53dba6ad93dcac148f511e743377c0d35bd7d
David Furnes <david@dfurnes.com>
2019-02-04 13:27:41 -0500

    Create a limited read-only SSM user for CircleCI.

--- shared/main.tf
+++ shared/main.tf
@@ -1,3 +1,34 @@
+data "aws_caller_identity" "current" {}
+
+data "aws_kms_alias" "default" {
+  name = "alias/aws/ssm"
+}
+
+# This is the IAM user & access key that grants CircleCI the ability
+# to read any secrets stored in the `/circleci/...` prefix in SSM.
+resource "aws_iam_user" "circleci" {
+  name = "circleci"
+}
+
+resource "aws_iam_user_policy" "circleci_policy" {
+  user   = "${aws_iam_user.circleci.name}"
+  policy = "${data.template_file.circleci_policy.rendered}"
+}
+
+data "template_file" "circleci_policy" {
+  template = "${file("${path.module}/circleci-policy.json.tpl")}"
+
+  vars {
+    kms_arn    = "${data.aws_kms_alias.default.arn}"
+    account_id = "${data.aws_caller_identity.current.account_id}"
+  }
+}
+
+resource "aws_iam_access_key" "circleci_key" {
+  user = "${aws_iam_user.circleci.name}"
+}
+
+# Heroku pipelines
 resource "heroku_pipeline" "graphql" {
   name = "graphql"
 }
