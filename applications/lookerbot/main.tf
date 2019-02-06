6148d649a6f5c01479296c550294be2211edfbea
Matt Holford <mholford@dosomething.org>
2019-02-06 16:31:59 -0500

    Updates Lookerbot setup comments.

--- applications/lookerbot/main.tf
+++ applications/lookerbot/main.tf
@@ -3,10 +3,16 @@
 # See https://github.com/looker/lookerbot#amazon-s3
 #
 # Manual setup steps:
-#   - Finally, head to the S3 Bucket in AWS after provisioning, and go to the
-#     Permissions -> Public Access Settings screen. Check all the options.
+#   - Head to <https://git.io/fh9DS> and hit "Deploy to Heroku" to provision the
+#     app.
+#   - Set the required environment variables via Heroku's admin panel.
+#     <https://git.io/fh9Dy>
+#   - Set the `SLACKBOT_S3_BUCKET`, `AWS_ACCESS_KEY_ID` &
+#     `AWS_SECRET_ACCESS_KEY` environment variables from the resources we've
+#     provisioned in this module.
 #
-# NOTE: We'll move more of these steps into Terraform over time!
+# NOTE: If this ends up being how we want to host & manage Lookerbot, we'll move
+# more of these steps into Terraform over time!
 
 # Required variables:
 variable "name" {
