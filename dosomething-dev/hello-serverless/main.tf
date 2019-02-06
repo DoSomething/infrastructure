79c02f1ae30c069c0e5631893cb86195e0d57223
David Furnes <david@dfurnes.com>
2019-02-06 18:23:38 -0500

    Uncomment!

--- dosomething-dev/hello-serverless/main.tf
+++ dosomething-dev/hello-serverless/main.tf
@@ -1,13 +1,15 @@
-# module "app" {
-#   source = "../../shared/lambda_function"
-#   name = "hello-serverless"
-# }
-# module "gateway" {
-#   source = "../../shared/api_gateway_proxy"
-#   name                = "hello-serverless"
-#   environment         = "development"
-#   function_arn        = "${module.app.arn}"
-#   function_invoke_arn = "${module.app.invoke_arn}"
-#   domain              = "hello-serverless.dosomething.org"
-# }
+module "app" {
+  source = "../../shared/lambda_function"
 
+  name = "hello-serverless"
+}
+
+module "gateway" {
+  source = "../../shared/api_gateway_proxy"
+
+  name                = "hello-serverless"
+  environment         = "development"
+  function_arn        = "${module.app.arn}"
+  function_invoke_arn = "${module.app.invoke_arn}"
+  domain              = "hello-serverless.dosomething.org"
+}
