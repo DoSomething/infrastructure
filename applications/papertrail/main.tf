aae7f95793475a9703b118751120ee05e3f285b3
David Furnes <david@dfurnes.com>
2019-02-06 11:24:59 -0500

    Grant CloudWatch permissions to forwarder function.

--- applications/papertrail/main.tf
+++ applications/papertrail/main.tf
@@ -22,6 +22,21 @@ module "forwarder" {
   }
 }
 
+resource "aws_lambda_permission" "allow_cloudwatch" {
+  statement_id  = "AllowExecutionFromCloudWatch"
+  action        = "lambda:InvokeFunction"
+  function_name = "${module.forwarder.name}"
+  principal     = "events.amazonaws.com"
+  source_arn    = "arn:aws:events:eu-west-1:111122223333:rule/RunDaily"
+  qualifier     = "${aws_lambda_alias.latest.name}"
+}
+
+resource "aws_lambda_alias" "latest" {
+  name             = "latest"
+  function_name    = "${module.forwarder.name}"
+  function_version = "$LATEST"
+}
+
 output "arn" {
   value = "${module.forwarder.arn}"
 }
