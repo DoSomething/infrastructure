cacde837cdd290a6ab5153696b7da2f7e36a61eb
David Furnes <david@dfurnes.com>
2019-02-07 11:34:00 -0500

    Remove example.

--- dosomething-dev/main.tf
+++ dosomething-dev/main.tf
@@ -95,10 +95,6 @@ module "papertrail" {
   papertrail_destination = "${var.papertrail_destination}"
 }
 
-module "example" {
-  source = "../applications/serverless-example"
-}
-
 module "ashes" {
   source = "ashes"
 }
