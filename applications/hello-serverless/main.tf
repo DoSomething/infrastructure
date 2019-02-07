f98cf9a723c0f3a95bc7f4c0bfde9dbd3420891d
David Furnes <david@dfurnes.com>
2019-02-07 16:31:33 -0500

    Set environment.

--- applications/hello-serverless/main.tf
+++ applications/hello-serverless/main.tf
@@ -14,6 +14,7 @@ module "gateway" {
   source = "../../shared/api_gateway_proxy"
 
   name                = "hello-serverless"
+  environment         = "development"
   function_arn        = "${module.app.arn}"
   function_invoke_arn = "${module.app.invoke_arn}"
 }
