48319d9f00f174f956898925f989cf078d977f59
David Furnes <david@dfurnes.com>
2019-04-04 15:54:46 -0400

    Eeek! Undo.

--- components/api_gateway/main.tf
+++ components/api_gateway/main.tf
@@ -91,7 +91,7 @@ resource "aws_api_gateway_deployment" "deployment" {
 
   # HACK: We need to re-trigger a deployment if the routes for this API Gateway
   # change. We can do so by modifying the stage description. <https://git.io/fjLs3>
-  stage_description = "Hash: ${md5(jsonencode(var.routes))}"
+  # stage_description = "Hash: ${md5(jsonencode(var.routes))}"
 }
 
 # Verify that the count matches the list <https://git.io/fjLYC>
