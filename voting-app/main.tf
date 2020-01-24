117a9adbead3ac783aed83a340bbeac2c737c73f
David Furnes <david@dfurnes.com>
2020-01-24 14:12:19 -0500

    Configure standalone 'voting-app' workspace.

--- voting-app/main.tf
+++ voting-app/main.tf
@@ -1,3 +1,26 @@
+variable "fastly_api_key" {}
+
+terraform {
+  backend "remote" {
+    organization = "dosomething"
+
+    workspaces {
+      name = "voting-app"
+    }
+  }
+}
+
+provider "aws" {
+  version = "2.30.0"
+  region  = "us-east-1"
+  profile = "terraform"
+}
+
+provider "fastly" {
+  version = "0.9.0"
+  api_key = var.fastly_api_key
+}
+
 resource "fastly_service_v1" "voting-app" {
   name          = "Terraform: Voting App"
   force_destroy = true
