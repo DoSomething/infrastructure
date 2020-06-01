7f65f1c11169694b142dd3e91d0a1777bb20d758
Sena Heydari <sheydari@dosomething.org>
2020-06-01 15:53:24 -0400

    Replace parameter group longform with PostgreSQL module source.

--- quasar/main.tf
+++ quasar/main.tf
@@ -30,305 +30,6 @@ module "lookerbot" {
   name = "dosomething-lookerbot"
 }
 
-resource "aws_db_parameter_group" "quasar-qa-pg11" {
-  name   = "quasar-qa-pg11"
-  family = "postgres11"
-
-  # Required for DMS CDC Replication:
-  # https://docs.aws.amazon.com/dms/latest/userguide/CHAP_Source.PostgreSQL.html#CHAP_Source.PostgreSQL.v10
-  parameter {
-    name         = "rds.logical_replication"
-    value        = "1"
-    apply_method = "pending-reboot"
-  }
-
-  # Required for DMS CDC Replication:
-  # https://docs.aws.amazon.com/dms/latest/userguide/CHAP_Source.PostgreSQL.html#CHAP_Source.PostgreSQL.v10
-  parameter {
-    name         = "max_logical_replication_workers"
-    value        = "500"
-    apply_method = "pending-reboot"
-  }
-
-  # Recommended by PGTuner tool: https://pgtune.leopard.in.ua/#/
-  # Sets effective RAM available to PG Query planner before using disk.
-  parameter {
-    name  = "effective_cache_size"
-    value = "12000000"
-  }
-
-  # Recommended by PGTuner tool: https://pgtune.leopard.in.ua/#/
-  # Amount of RAM available for cleanup tasks like vacuum, reindex, etc.
-  parameter {
-    name  = "maintenance_work_mem"
-    value = "2000000"
-  }
-
-  # Recommended by PGTuner tool: https://pgtune.leopard.in.ua/#/
-  # Amount of RAM available for joins/sort queries per connection.
-  # Based on 50 connections.
-  parameter {
-    name  = "work_mem"
-    value = "41943"
-  }
-
-  # Recommended to only allow SSL connections from clients.
-  parameter {
-    name  = "rds.force_ssl"
-    value = "1"
-  }
-
-  # Only log queries with a duration longer than this to get slow queries.
-  parameter {
-    name  = "log_min_duration_statement"
-    value = "1000"
-  }
-
-  # Only log slow queries.
-  parameter {
-    name  = "log_statement"
-    value = "none"
-  }
-
-  parameter {
-    name  = "log_duration"
-    value = "0"
-  }
-
-  # Testing turning synchronization off to see if improves performance
-  parameter {
-    name  = "synchronous_commit"
-    value = "off"
-  }
-
-  # Enabling for PG Badger query tuning analysis.
-  parameter {
-    name  = "log_connections"
-    value = "1"
-  }
-
-  # Enabling for PG Badger query tuning analysis.
-  parameter {
-    name  = "log_disconnections"
-    value = "1"
-  }
-
-  # Enabling for PG Badger query tuning analysis.
-  parameter {
-    name  = "log_lock_waits"
-    value = "1"
-  }
-
-  # Enabling for PG Badger query tuning analysis.
-  parameter {
-    name  = "log_temp_files"
-    value = "0"
-  }
-
-  # Enabling for PG Badger query tuning analysis.
-  parameter {
-    name  = "log_autovacuum_min_duration"
-    value = "0"
-  }
-}
-
-resource "aws_db_parameter_group" "quasar-qa-pg12" {
-  name   = "quasar-qa-pg12"
-  family = "postgres12"
-
-  # Required for DMS CDC Replication:
-  # https://docs.aws.amazon.com/dms/latest/userguide/CHAP_Source.PostgreSQL.html#CHAP_Source.PostgreSQL.v10
-  parameter {
-    name         = "rds.logical_replication"
-    value        = "1"
-    apply_method = "pending-reboot"
-  }
-
-  # Required for DMS CDC Replication:
-  # https://docs.aws.amazon.com/dms/latest/userguide/CHAP_Source.PostgreSQL.html#CHAP_Source.PostgreSQL.v10
-  parameter {
-    name         = "max_logical_replication_workers"
-    value        = "500"
-    apply_method = "pending-reboot"
-  }
-
-  # Recommended by PGTuner tool: https://pgtune.leopard.in.ua/#/
-  # Sets effective RAM available to PG Query planner before using disk.
-  parameter {
-    name  = "effective_cache_size"
-    value = 12 * 1000 * 1000 # 12GB
-  }
-
-  # Recommended by PGTuner tool: https://pgtune.leopard.in.ua/#/
-  # Amount of RAM available for cleanup tasks like vacuum, reindex, etc.
-  parameter {
-    name  = "maintenance_work_mem"
-    value = 2 * 1000 * 1000 # 2GB
-  }
-
-  # Recommended by PGTuner tool: https://pgtune.leopard.in.ua/#/
-  # Amount of RAM available for joins/sort queries per connection.
-  parameter {
-    name  = "work_mem"
-    value = "26214"
-  }
-
-  # Recommended to only allow SSL connections from clients.
-  parameter {
-    name  = "rds.force_ssl"
-    value = "1"
-  }
-
-  # Only log queries with a duration longer than this to get slow queries.
-  parameter {
-    name  = "log_min_duration_statement"
-    value = "1000" # 1 second, measured in milliseconds
-  }
-
-  # Only log slow queries.
-  parameter {
-    name  = "log_statement"
-    value = "none"
-  }
-
-  parameter {
-    name  = "log_duration"
-    value = "0"
-  }
-
-  # Enabling for PG Badger query tuning analysis.
-  parameter {
-    name  = "log_connections"
-    value = "1"
-  }
-
-  # Enabling for PG Badger query tuning analysis.
-  parameter {
-    name  = "log_disconnections"
-    value = "1"
-  }
-
-  # Enabling for PG Badger query tuning analysis.
-  parameter {
-    name  = "log_lock_waits"
-    value = "1"
-  }
-
-  # Enabling for PG Badger query tuning analysis.
-  parameter {
-    name  = "log_temp_files"
-    value = "0"
-  }
-
-  # Enabling for PG Badger query tuning analysis.
-  parameter {
-    name  = "log_autovacuum_min_duration"
-    value = "0"
-  }
-}
-
-resource "aws_db_parameter_group" "quasar-prod-pg11" {
-  name   = "quasar-prod-pg11"
-  family = "postgres11"
-
-  # Required for DMS CDC Replication:
-  # https://docs.aws.amazon.com/dms/latest/userguide/CHAP_Source.PostgreSQL.html#CHAP_Source.PostgreSQL.v10
-  parameter {
-    name         = "rds.logical_replication"
-    value        = "1"
-    apply_method = "pending-reboot"
-  }
-
-  # Required for DMS CDC Replication:
-  # https://docs.aws.amazon.com/dms/latest/userguide/CHAP_Source.PostgreSQL.html#CHAP_Source.PostgreSQL.v10
-  parameter {
-    name         = "max_logical_replication_workers"
-    value        = "500"
-    apply_method = "pending-reboot"
-  }
-
-  # Recommended by PGTuner tool: https://pgtune.leopard.in.ua/#/
-  # Sets effective RAM available to PG Query planner before using disk.
-  parameter {
-    name  = "effective_cache_size"
-    value = "48000000"
-  }
-
-  # Recommended by PGTuner tool: https://pgtune.leopard.in.ua/#/
-  # Amount of RAM available for cleanup tasks like vacuum, reindex, etc.
-  parameter {
-    name  = "maintenance_work_mem"
-    value = "2000000"
-  }
-
-  # Updated to match Heroku Premium-5 config which we experienced
-  # as more performant when our data warehouse was hosted there.
-  # Amount of RAM available for joins/sort queries per connection.
-  parameter {
-    name  = "work_mem"
-    value = "120000"
-  }
-
-  # Recommended to only allow SSL connections from clients.
-  parameter {
-    name  = "rds.force_ssl"
-    value = "1"
-  }
-
-  # Only log queries with a duration longer than this to get slow queries.
-  parameter {
-    name  = "log_min_duration_statement"
-    value = "1000"
-  }
-
-  # Temporarily only log slow queries.
-  parameter {
-    name  = "log_statement"
-    value = "none"
-  }
-
-  parameter {
-    name  = "log_duration"
-    value = "0"
-  }
-
-  # Set to default value globally per https://git.io/fjghW.
-  parameter {
-    name  = "statement_timeout"
-    value = "0"
-  }
-
-  # Enabling for PG Badger query tuning analysis.
-  parameter {
-    name  = "log_connections"
-    value = "1"
-  }
-
-  # Enabling for PG Badger query tuning analysis.
-  parameter {
-    name  = "log_disconnections"
-    value = "1"
-  }
-
-  # Enabling for PG Badger query tuning analysis.
-  parameter {
-    name  = "log_lock_waits"
-    value = "1"
-  }
-
-  # Enabling for PG Badger query tuning analysis.
-  parameter {
-    name  = "log_temp_files"
-    value = "0"
-  }
-
-  # Enabling for PG Badger query tuning analysis.
-  parameter {
-    name  = "log_autovacuum_min_duration"
-    value = "0"
-  }
-}
-
 data "aws_ssm_parameter" "qa_username" {
   name = "/quasar-qa/rds/username"
 }
@@ -346,8 +47,7 @@ data "aws_ssm_parameter" "prod_password" {
 }
 
 # Trying to setup network resources necessary to remove existing
-# longform value above from components/vpc module.
-
+# longform values above and source from components/vpc module.
 variable "rds_sec_group" {
   description = "Security group for Quasar RDS instances."
 }
@@ -356,6 +56,24 @@ module "network" {
   source = "../components/vpc"
 
   rds_sec_group = module.aws_security_group.rds.id
+}
+
+# Trying to setup parameter groups necessry to remove existing 
+# longform values above and source from components/postgresql_instance module.
+
+variable "qa_param_group" {
+  description = "PostgreSQL parameter group for Quasar QA RDS Instance."
+}
+
+variable "prod_param_group" {
+  description = "PostgreSQL parameter group for Quasar Prod RDS Instance."
+}
+
+module "parameters" {
+  source = "../components/postgresql_instance"
+
+  qa_param_group   = module.aws_db_parameter_group.quasar-qa-pg11.id
+  prod_param_group = module.aws_db_parameter_group.quasar-prod-pg11.id
 }
 
 resource "aws_db_instance" "quasar-qa" {
@@ -367,7 +85,7 @@ resource "aws_db_instance" "quasar-qa" {
   name                            = "quasar"
   username                        = data.aws_ssm_parameter.qa_username.value
   password                        = data.aws_ssm_parameter.qa_password.value
-  parameter_group_name            = aws_db_parameter_group.quasar-qa-pg11.id
+  parameter_group_name            = var.qa_param_group
   vpc_security_group_ids          = var.rds_sec_group
   deletion_protection             = true
   storage_encrypted               = true
@@ -386,7 +104,7 @@ resource "aws_db_instance" "quasar" {
   name                            = "quasar_prod_warehouse"
   username                        = data.aws_ssm_parameter.prod_username.value
   password                        = data.aws_ssm_parameter.prod_password.value
-  parameter_group_name            = aws_db_parameter_group.quasar-prod-pg11.id
+  parameter_group_name            = var.prod_param_group
   vpc_security_group_ids          = var.rds_sec_group
   deletion_protection             = true
   storage_encrypted               = true
@@ -396,7 +114,6 @@ resource "aws_db_instance" "quasar" {
   performance_insights_enabled    = true
   enabled_cloudwatch_logs_exports = ["postgresql", "upgrade"]
 }
-
 
 # Provide S3 Bucket for Customer.io data file exports.
 
