4a4085418de4949e82f40533c3480755d8d70b53
David Furnes <david@dfurnes.com>
2020-06-03 12:43:43 -0400

    Add link to PR with context.

--- components/iam_app_user/main.tf
+++ components/iam_app_user/main.tf
@@ -6,7 +6,7 @@ resource "aws_iam_user" "aws_user" {
   name = var.name
 
   # When destroying this resource, destroy even if it has non-Terraform
-  # managed access keys, policies, etc. See:
+  # managed access keys, policies, etc. See: https://git.io/JfP7O
   force_destroy = true
 }
 
