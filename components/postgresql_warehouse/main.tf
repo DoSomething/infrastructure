5cffd541f648293c4ff29af526f23f9e31420a07
Sena Heydari <sheyd@users.noreply.github.com>
2020-06-04 15:31:38 -0400

    Adding separate stub files and VPC configuration for Quasar. (#251)
    * Adding separate stub files and VPC configuration for Quasar.
    
    * Adding DB parameters in separate postgresql_instance module.
    
    * Remove main file network block and replace with VPC module.
    
    * Replace parameter group longform with PostgreSQL module source.
    
    * Attempt to set var defaults before they get assigned real values.
    
    * Removed vars and added internal values to see if works.
    
    * Trying most basic import syntax to figure out what I'm doing wrong.
    
    * Rename 'vpc' to 'quasar_vpc'.
    
    * Move database into a component that can be reused.
    
    * Create parameter groups in the component.
    
    * Remove 'description' argument.
    
    * Remove reference to unused module.
    
    * Update reference to renamed input variable.
    
    * Update type to list of strings (not single string ID).
    
    * Fix configuration drift between QA & Prod.
    
    We're keeping prod settings (and applying them to QA where inconsistent).
    
    * Allow different engine versions between prod & QA.
    
    Co-authored-by: David Furnes <david@dfurnes.com>

---
+++ components/postgresql_warehouse/main.tf
@@ -0,0 +1,171 @@
+variable "database_name" {
+  description = "The name used for this database."
+}
+
+variable "engine_version" {
+  description = "The PostgreSQL version to use for this instance."
+}
+
+variable "parameter_group_name" {
+  description = "The name used for this database's parameter group."
+}
+
+variable "allocated_storage" {
+  description = "The amount of storage (in GB) to allocate for this instance."
+}
+
+variable "instance_class" {
+  description = "The RDS instance type for this database."
+}
+
+variable "username" {
+  description = "The master username for the RDS PostgreSQL instance."
+}
+
+variable "password" {
+  description = "The master password for the RDS PostgreSQL instance."
+}
+
+variable "effective_cache_size" {
+  description = "Effective RAM available to PG Query planner before using disk."
+}
+
+variable "maintenance_work_mem" {
+  description = "Amount of RAM available for cleanup tasks like vacuum, reindex, etc."
+}
+
+variable "work_mem" {
+  description = "Amount of RAM available for joins/sort queries per connection."
+}
+
+variable "vpc_security_group_ids" {
+  description = "The VPC security group IDs to grant access to this database."
+}
+
+resource "aws_db_parameter_group" "pg11" {
+  name   = var.parameter_group_name
+  family = "postgres11"
+
+  # Required for DMS CDC Replication:
+  # https://docs.aws.amazon.com/dms/latest/userguide/CHAP_Source.PostgreSQL.html#CHAP_Source.PostgreSQL.v10
+  parameter {
+    name         = "rds.logical_replication"
+    value        = "1"
+    apply_method = "pending-reboot"
+  }
+
+  # Required for DMS CDC Replication:
+  # https://docs.aws.amazon.com/dms/latest/userguide/CHAP_Source.PostgreSQL.html#CHAP_Source.PostgreSQL.v10
+  parameter {
+    name         = "max_logical_replication_workers"
+    value        = "500"
+    apply_method = "pending-reboot"
+  }
+
+  # Recommended by PGTuner tool: https://pgtune.leopard.in.ua/#/
+  # Sets effective RAM available to PG Query planner before using disk.
+  # TODO: It'd be great to make this value dynamic by accessing RAM from
+  # instance type var and having something like (rds.ram_value * .75) * 1000 * 1000. 
+  parameter {
+    name  = "effective_cache_size"
+    value = var.effective_cache_size
+  }
+
+  # Recommended by PGTuner tool: https://pgtune.leopard.in.ua/#/
+  # Amount of RAM available for cleanup tasks like vacuum, reindex, etc.
+  # TODO: It'd be great to make this value dynamic by accessing RAM from
+  # instance type var and having something like (rds.ram_value * .125) * 1000 * 1000. 
+  parameter {
+    name  = "maintenance_work_mem"
+    value = var.maintenance_work_mem
+  }
+
+  # Recommended by PGTuner tool: https://pgtune.leopard.in.ua/#/
+  # Amount of RAM available for joins/sort queries per connection.
+  # Based on 50 connections.
+  # TODO: Not sure we can dynamically generate this, but maybe set a default
+  # value and override per QA/Prod environment. 
+  parameter {
+    name  = "work_mem"
+    value = var.work_mem
+  }
+
+  # Recommended to only allow SSL connections from clients.
+  parameter {
+    name  = "rds.force_ssl"
+    value = "1"
+  }
+
+  # Only log queries with a duration longer than this to get slow queries.
+  parameter {
+    name  = "log_min_duration_statement"
+    value = "1000"
+  }
+
+  # Only log slow queries.
+  parameter {
+    name  = "log_statement"
+    value = "none"
+  }
+
+  parameter {
+    name  = "log_duration"
+    value = "0"
+  }
+
+  parameter {
+    apply_method = "immediate"
+    name         = "statement_timeout"
+    value        = "0"
+  }
+
+  # Enabling for PG Badger query tuning analysis.
+  parameter {
+    name  = "log_connections"
+    value = "1"
+  }
+
+  # Enabling for PG Badger query tuning analysis.
+  parameter {
+    name  = "log_disconnections"
+    value = "1"
+  }
+
+  # Enabling for PG Badger query tuning analysis.
+  parameter {
+    name  = "log_lock_waits"
+    value = "1"
+  }
+
+  # Enabling for PG Badger query tuning analysis.
+  parameter {
+    name  = "log_temp_files"
+    value = "0"
+  }
+
+  # Enabling for PG Badger query tuning analysis.
+  parameter {
+    name  = "log_autovacuum_min_duration"
+    value = "0"
+  }
+}
+
+resource "aws_db_instance" "quasar" {
+  allocated_storage               = var.allocated_storage
+  engine                          = "postgres"
+  engine_version                  = var.engine_version
+  allow_major_version_upgrade     = true
+  instance_class                  = var.instance_class
+  name                            = var.database_name
+  username                        = var.username
+  password                        = var.password
+  parameter_group_name            = aws_db_parameter_group.pg11.id
+  vpc_security_group_ids          = var.vpc_security_group_ids
+  deletion_protection             = true
+  storage_encrypted               = true
+  copy_tags_to_snapshot           = true
+  monitoring_interval             = "10"
+  publicly_accessible             = true
+  performance_insights_enabled    = true
+  enabled_cloudwatch_logs_exports = ["postgresql", "upgrade"]
+}
