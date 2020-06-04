16055716a11e6240c84a46b0f81d257e485233a1
Morgan Rich <mrich@dosomething.org>
2020-06-04 14:50:25 -0400

    Adding default value for fivetran_cloudwatch_integration_external_id var

--- components/fivetran_cloudwatch_role/variables.tf
+++ components/fivetran_cloudwatch_role/variables.tf
@@ -1,3 +1,4 @@
 variable "fivetran_cloudwatch_integration_external_id" {
   description = "External ID for the trust relationship between fivetran and AWS"
+  default = var.fivetran_cloudwatch_integration_external_id
 }
