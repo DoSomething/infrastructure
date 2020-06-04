434647569b0e2c3246980df486ad7b950e94a313
David Furnes <david@dfurnes.com>
2020-06-04 14:27:39 -0400

    Remove 'description' argument.

--- components/quasar_vpc/main.tf
+++ components/quasar_vpc/main.tf
@@ -1,28 +1,28 @@
 # This is the Quasar VPC layout.
 
+# Quasar VPC IP Range:
 resource "aws_vpc" "quasar_vpc" {
-  description = "Quasar VPC IP range."
-  cidr_block  = "10.255.0.0/16"
+  cidr_block = "10.255.0.0/16"
 
   tags = {
     Name = "Quasar"
   }
 }
 
+# 1st RDS subnet for multi-AZ DB setup.
 resource "aws_subnet" "subnet-a" {
-  description = "1st RDS subnet for multi-AZ DB setup."
-  vpc_id      = aws_vpc.quasar_vpc.id
-  cidr_block  = "10.255.100.0/24"
+  vpc_id     = aws_vpc.quasar_vpc.id
+  cidr_block = "10.255.100.0/24"
 
   tags = {
     Name = "Quasar RDS - 1A"
   }
 }
 
+# 2nd RDS subnet for multi-AZ DB setup.
 resource "aws_subnet" "subnet-b" {
-  description = "2nd RDS subnet for multi-AZ DB setup."
-  vpc_id      = aws_vpc.quasar_vpc.id
-  cidr_block  = "10.255.101.0/24"
+  vpc_id     = aws_vpc.quasar_vpc.id
+  cidr_block = "10.255.101.0/24"
 
   tags = {
     Name = "Quasar RDS - 1E"
