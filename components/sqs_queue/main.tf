d90bfdcb4e326ac0a0f004921315cec2853e6856
Diego Lorenzo <diego@diegolorenzo.com>
2020-08-05 16:05:25 -0400

    Updating env variables after changes due to Rogue updates. (#267)
    * Updating env variables after changes due to Rogue updates.
    
    * Updating the access key env vars as well.
    
    * Copy-pasta typo. Thanks terraform error reporting.

--- components/sqs_queue/main.tf
+++ components/sqs_queue/main.tf
@@ -50,6 +50,10 @@ output "id" {
 
 output "config_vars" {
   value = {
+    AWS_SQS_QUEUE     = aws_sqs_queue.queue.id
+    QUEUE_CONNECTION  = "sqs"
+
+    # @TODO: Remove these old vars once safe to do so!
     QUEUE_DRIVER      = "sqs"
     SQS_DEFAULT_QUEUE = aws_sqs_queue.queue.id
   }
