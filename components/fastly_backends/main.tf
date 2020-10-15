81dafae5184727428f87cf07d8e4e04730648a94
David Furnes <david@dfurnes.com>
2020-10-15 11:11:28 -0400

    Update Pivotal ticket link.

--- components/fastly_backends/main.tf
+++ components/fastly_backends/main.tf
@@ -32,7 +32,7 @@ locals {
     # when crossing domains, and nothing at all when going to HTTP.
     "Referrer-Policy" = "\"strict-origin-when-cross-origin\""
 
-    # Enforce HTTPS for all traffic (TODO: https://www.pivotaltracker.com/story/show/174911098)
+    # Enforce HTTPS for all traffic (TODO: https://www.pivotaltracker.com/story/show/175288875)
     "Strict-Transport-Security" = "\"max-age=604800; includeSubDomains\"" # 1 week.
 
     # Prevent sniffing content types:
