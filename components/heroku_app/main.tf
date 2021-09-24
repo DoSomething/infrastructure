e619909fc9371ea01d6ab9594eb2dc71db17d4a3
David Furnes <david@dfurnes.com>
2021-09-24 11:44:18 -0400

    Document and extract variables/outputs for heroku_app.

--- components/heroku_app/main.tf
+++ components/heroku_app/main.tf
@@ -1,75 +1,3 @@
-# Required variables:
-variable "environment" {
-  description = "The environment for this application: development, qa, or production."
-}
-
-variable "pipeline" {
-  description = "The Heroku pipeline ID this application should be created in."
-}
-
-variable "name" {
-  description = "The application name."
-}
-
-variable "framework" {
-  description = "The framework for this application (e.g. 'laravel', 'express')."
-}
-
-variable "papertrail_destination" {
-  description = "The Papertrail log destination for this application."
-}
-
-variable "domain" {
-  description = "The domain this application will be accessible at, e.g. longshot.dosomething.org"
-}
-
-# Optional variables:
-variable "web_size" {
-  description = "The Heroku dyno type for web processes."
-  default     = "Standard-1X"
-}
-
-variable "web_scale" {
-  description = "The number of web dynos this application should have."
-  default     = "1"
-}
-
-variable "ignore_web" {
-  description = "Should we manage the web dyno with Terraform? Set to 'true' if auto-scaling."
-  default     = false
-}
-
-variable "queue_size" {
-  description = "The Heroku dyno type for queue processes."
-  default     = "Standard-1X"
-}
-
-variable "queue_scale" {
-  description = "The number of queue dynos this application should have."
-  default     = "1"
-}
-
-variable "config_vars" {
-  description = "Environment variables for this application."
-  default     = {}
-}
-
-variable "with_newrelic" {
-  description = "Should New Relic be configured for this app? Generally only used on prod."
-  default     = false
-}
-
-variable "with_redis" {
-  description = "Should we create a Redis store for this application?"
-  default     = false
-}
-
-variable "redis_type" {
-  # See usage below for default value. <https://stackoverflow.com/a/51758050/811624>
-  description = "The Heroku Redis add-on plan. See: https://goo.gl/3v3RXX"
-  default     = ""
-}
-
 data "aws_ssm_parameter" "newrelic_api_key" {
   count = var.with_newrelic ? 1 : 0
   name  = "/newrelic/api-key"
@@ -204,8 +132,4 @@ resource "heroku_addon" "redis" {
   count = var.with_redis ? 1 : 0
   app   = heroku_app.app.name
   plan  = "heroku-redis:${coalesce(var.redis_type, local.redis_default)}"
-}
-
-output "backend" {
-  value = heroku_app.app.heroku_hostname
 }
